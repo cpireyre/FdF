8f4b0c1432c11657cb38fc636de2fe542aa1cb5e
Colin Pireyre <copireyr@student.hive.fi>
2024-05-25 16:30:46 +0300

    add escape hook to close window

--- src/render.c
+++ src/render.c
@@ -6,16 +6,26 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 11:43:04 by copireyr          #+#    #+#             */
-/*   Updated: 2024/05/25 16:02:07 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/05/25 16:28:24 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/MLX42.h"
 
+static void	key_hooks(mlx_key_data_t key_data, void *param);
+
 void	render(void)
 {
 	mlx_t	*mlx;
 
-	mlx_init(600, 480, "meme lib", true);
+	mlx = mlx_init(600, 480, "meme lib", true);
+	mlx_key_hook(mlx, &key_hooks, mlx);
 	mlx_loop(mlx);
+	mlx_terminate(mlx);
+}
+
+static void	key_hooks(mlx_key_data_t key_data, void *param)
+{
+	if (key_data.key == MLX_KEY_ESCAPE)
+		mlx_close_window((mlx_t *)param);
 }
