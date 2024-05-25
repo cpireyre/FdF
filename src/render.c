4a78470db7e32b1c932f6cfba6530f4639f98fa3
Colin Pireyre <copireyr@student.hive.fi>
2024-05-25 16:37:17 +0300

    check error on mlx init

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 11:43:04 by copireyr          #+#    #+#             */
-/*   Updated: 2024/05/25 16:28:24 by copireyr         ###   ########.fr       */
+/*   Updated: 2024/05/25 16:37:07 by copireyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ void	render(void)
 {
 	mlx_t	*mlx;
 
-	mlx = mlx_init(600, 480, "meme lib", true);
+	mlx = mlx_init(600, 480, "FdF", true);
+	if (!mlx)
+		return (1);
 	mlx_key_hook(mlx, &key_hooks, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
+	return (0);
 }
 
 static void	key_hooks(mlx_key_data_t key_data, void *param)
