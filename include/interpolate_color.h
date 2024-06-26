2438dbc5659e31432dc7bc8ac538e86576b2760c
Colin Pireyre <copireyr@student.hive.fi>
2024-06-26 14:53:55 +0300

    fix norm

--- include/interpolate_color.h
+++ include/interpolate_color.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   interpolate_color.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 14:49:59 by copireyr          #+#    #+#             */
+/*   Updated: 2024/06/26 14:53:21 by copireyr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INTERPOLATE_COLOR_H
 # define INTERPOLATE_COLOR_H
 
@@ -6,14 +18,18 @@
 
 extern uint32_t	interpolate_color(uint32_t start, uint32_t end, double lerp);
 
-typedef union u_color {
-    struct {
-        uint8_t r;
-        uint8_t g;
-        uint8_t b;
-        uint8_t a;
-    } ch;
-    uint32_t value;
-} t_color;
+struct s_ch
+{
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+	uint8_t	a;
+};
+
+typedef union u_color
+{
+	struct s_ch	ch;
+	uint32_t	value;
+}	t_color;
 
 #endif /* INTERPOLATE_COLOR_H */
--- src/rasterize.c
+++ src/rasterize.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rasterize.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: copireyr <copireyr@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 13:36:06 by copireyr          #+#    #+#             */
+/*   Updated: 2024/06/26 14:49:43 by copireyr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rasterize.h"
 #include "libft.h"
 
-static void	connect(mlx_image_t *img, t_vector a, t_vector b);
-static void	bresenham(mlx_image_t *img, t_line line);
+static void		connect(mlx_image_t *img, t_vector a, t_vector b);
+static void		bresenham(mlx_image_t *img, t_line line);
+static uint32_t	lerp(uint32_t color0, uint32_t color1, int i, int steps);
 
 void	rasterize(mlx_image_t *image, t_map *map)
 {
@@ -31,9 +44,9 @@ static void	connect(mlx_image_t *img, t_vector a, t_vector b)
 {
 	t_line	line;
 	double	original_length;
-	double	start_proportion;
-	double	end_proportion;
-	
+	double	ratio0;
+	double	ratio1;
+
 	line.x0 = a.x;
 	line.y0 = a.y;
 	line.x1 = b.x;
@@ -43,16 +56,16 @@ static void	connect(mlx_image_t *img, t_vector a, t_vector b)
 	original_length = ft_distance(a.x, a.y, b.x, b.y);
 	if (clip(&line, (int)img->width, (int)img->height))
 	{
-	    start_proportion = ft_distance(a.x, a.y, line.x0, line.y0) / original_length;
-	    end_proportion = ft_distance(a.x, a.y, line.x1, line.y1) / original_length;
-	    line.color0 = interpolate_color((uint32_t)a.c, (uint32_t)b.c, start_proportion);
-	    line.color1 = interpolate_color((uint32_t)a.c, (uint32_t)b.c, end_proportion);
-	    line.delta_x = ft_abs(line.x1 - line.x0);
-	    line.delta_y = -ft_abs(line.y1 - line.y0);
-	    line.slope_x = ft_sign(line.x0, line.x1);
-	    line.slope_y = ft_sign(line.y0, line.y1);
-	    line.length = ft_max(line.delta_x, -line.delta_y);
-	    bresenham(img, line);
+		ratio0 = ft_distance(a.x, a.y, line.x0, line.y0) / original_length;
+		ratio1 = ft_distance(a.x, a.y, line.x1, line.y1) / original_length;
+		line.color0 = interpolate_color((uint32_t)a.c, (uint32_t)b.c, ratio0);
+		line.color1 = interpolate_color((uint32_t)a.c, (uint32_t)b.c, ratio1);
+		line.delta_x = ft_abs(line.x1 - line.x0);
+		line.delta_y = -ft_abs(line.y1 - line.y0);
+		line.slope_x = ft_sign(line.x0, line.x1);
+		line.slope_y = ft_sign(line.y0, line.y1);
+		line.length = ft_max(line.delta_x, -line.delta_y);
+		bresenham(img, line);
 	}
 }
 
@@ -64,13 +77,12 @@ static void	bresenham(mlx_image_t *img, t_line line)
 	int			double_err;
 
 	i = 0;
-	color = line.color0;
 	err = line.delta_x + line.delta_y;
 	while (line.x0 != line.x1 || line.y0 != line.y1)
 	{
 		double_err = err * 2;
 		mlx_put_pixel(img, (uint32_t)line.x0, (uint32_t)line.y0, color);
-		color = interpolate_color(line.color0, line.color1, (double)i/(double)line.length);
+		color = lerp(line.color0, line.color1, i, line.length);
 		if (double_err > line.delta_y)
 		{
 			err += line.delta_y;
@@ -84,4 +96,10 @@ static void	bresenham(mlx_image_t *img, t_line line)
 		i++;
 	}
 	mlx_put_pixel(img, (uint32_t)line.x1, (uint32_t)line.y1, line.color1);
+}
+
+static uint32_t	lerp(uint32_t color0, uint32_t color1, int i, int steps)
+{
+	return ((uint32_t)round(interpolate_color(color0, color1,
+				(double) i / (double)steps)));
 }
