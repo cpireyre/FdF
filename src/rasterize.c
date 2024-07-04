dcf880005c8a4e71b668168cafefa02c49e49516
Colin <cpireyre@student.42.fr>
2024-07-04 17:33:38 +0300

    don't draw lines of length 0

--- src/rasterize.c
+++ src/rasterize.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "rasterize.h"
+#include<stdlib.h>
 
 static void		bresenham(mlx_image_t *img, int *z_buffer, t_line line);
 static inline void	plot(mlx_image_t *img, t_v2i a, uint32_t color);
@@ -20,14 +21,14 @@ void	rasterize(mlx_image_t *image, int *z_buffer, t_line line)
 {
 	line.delta.x = ft_abs(line.screen1.x - line.screen0.x);
 	line.delta.y = -ft_abs(line.screen1.y - line.screen0.y);
-	line.slope.x = ft_sign(line.screen0.x, line.screen1.x);
-	line.slope.y = ft_sign(line.screen0.y, line.screen1.y);
 	line.length = ft_max(line.delta.x, -line.delta.y);
 	if (line.length)
+	{
+		line.slope.x = ft_sign(line.screen0.x, line.screen1.x);
+		line.slope.y = ft_sign(line.screen0.y, line.screen1.y);
 		line.z_dir = (int)round((line.world1.z - line.world0.z) /  line.length);
-	else
-		line.z_dir = 0;
-	bresenham(image, z_buffer, line);
+		bresenham(image, z_buffer, line);
+	}
 }
 
 static void	bresenham(mlx_image_t *img, int *z_buffer, t_line line)
